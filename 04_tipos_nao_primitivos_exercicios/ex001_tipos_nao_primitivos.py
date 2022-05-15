da894bd4fb44ae32c03c17516b2e2158ceef9a8f
Lia Pires <liasth.pires@gmail.com>
2022-05-15 11:27:28 -0300

    Revisão
    Revisão do nome das pastas

---
+++ 04_tipos_nao_primitivos_exercicios/ex001_tipos_nao_primitivos.py
@@ -0,0 +1,4 @@
+# Escreva um programa que some todos os itens de uma lista.
+
+lista =
+
--- exercicios_estruturas_de_repeticao/ex007_estruturas_de_repeticao.py
+++
@@ -1,7 +0,0 @@
-'''Escrever um algoritmo que leia um valor para uma variável N de 1 a 10 e calcule a tabuada de
-N. Mostre a tabuada na forma: 0 x N = 0, 1 x N = 1N, 2 x N = 2N, ..., 10 x N = 10N.'''
-
-N = int(input("Digite um número inteiro para ter sua tabuada: "))
-
-for i in range(1,11):
-    print(f"{N} x {i} = {N*(i)}")
