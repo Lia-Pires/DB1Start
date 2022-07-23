901b0c9cfe7a31eb2afa8832486d4badcd288c16
Lia Pires <liasth.pires@gmail.com>
2022-07-23 18:48:02 -0300

    Update ex008 and ex009
    Update ex008 and ex009

---
+++ 06_classes/ex008_classes.py
@@ -0,0 +1,13 @@
+'''
+Criar uma classe Musica, que deverá receber como
+parâmetro de criação uma lista de string com as
+estrofes de uma música. Esta classe deverá conter um
+método chamado cante_para_mim que imprima todas
+as estrofes, uma em cada linha.
+Instanciar a classe Triangulo e imprimir os lados
+informados e imprimir se o triângulo é um triângulo ou não.
+O método check_angulo deverá retornar True se a
+soma dos valores for igual a 180 e False em qualquer outra
+possibilidade;
+Para executar, instancie a classe e execute o método.
+'''
