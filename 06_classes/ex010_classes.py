f759935929a9cc04a597efb1f1fc9994301640ef
Lia Pires <liasth.pires@gmail.com>
2022-07-23 18:49:30 -0300

    Update Ex010 and Ex011
    Update Ex010 and Ex011

---
+++ 06_classes/ex010_classes.py
@@ -0,0 +1,22 @@
+'''
+Criar uma classe que representará uma aeronave militar com
+o nome JatoMilitar1Lugar. O método construtor deverá receber
+duas informações iniciais: o modelo e a base onde a aeronave
+está; Crie um método chamado designar_piloto, que deverá
+receber o nome do piloto como parâmetro
+
+Criar um método rebasear_aeronave, que deverá receber de
+parâmetro o nome da base para onde a aeronave deverá ser
+enviada. Antes de registrar, é necessário validar se o piloto foi
+designado para a aeronave, e somente se houver piloto, registrar
+a informação de rebaseamento, a data e hora que a
+movimentação foi realizada, deverá ser registrada também.
+Sobrescreva o métod __str__ da classe, para imprimir o seguinte
+conteúdo:
+Jato: <NOME DA AERONAVE>
+Base inicial: <NOME DA BASE DE ORIGEM>
+Piloto: <NOME DO PILOTO DESIGNADO>
+Histórico de transferências: <LISTAGEM DAS BASES PELAS QUAIS A
+AERONAVE PASSOU MOSTRANDO A DATA / HORA E BASE>
+
+'''
---
+++ 06_classes/ex011_classes.py
@@ -0,0 +1,26 @@
+'''
+Criar uma classe que representará outro tipo de aeronave
+militar, com o nome JatoMilitar2Lugares. O método construtor
+deverá receber duas informações: o modelo, e a base onde a
+aeronave está; Cria um método chamado designar_piloto que
+everá contar o nome do piloto. Por regra, o primeiro piloto
+será considerado o piloto principal e o segundo, o seu copiloto. Depois de informados os dois nomes, não poderá ser
+inserido mais nenhum nome.
+
+
+Criar um método rebasear_aeronave, que receberá como
+parâmetro o nome da base para onde a aeronave deverá ser
+enviada. Antes de registrar, é necessário validar se dois pilotos
+foram designados para a aeronave, e somente se ambos tiverem
+sido designados, registrar o rebaseamento, com a data e hora da
+movimentação.
+
+Sobrescreva o métod __str__ da classe, para imprimir o seguinte
+conteúdo:
+Jato: <NOME DA AERONAVE>
+Base inicial: <NOME DA BASE DE ORIGEM>
+Piloto: <NOME DOS PILOTOS DESIGNADOS>
+Histórico de transferências: <LISTAGEM DAS BASES PELAS QUAIS A
+AERONAVE PASSOU MOSTRANDO A DATA / HORA E BASE>
+
+'''
