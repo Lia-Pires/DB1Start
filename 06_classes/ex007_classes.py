85ea42c1133f46c10e2af15bd1797c2f4652989f
Lia Pires <liasth.pires@gmail.com>
2022-07-28 09:24:00 -0300

    Update ex_007_classes.py
    Update ex_007_classes.py

--- 06_classes/ex007_classes.py
+++ 06_classes/ex007_classes.py
@@ -11,3 +11,25 @@ soma dos valores for igual a 180 e False em qualquer outra
 possibilidade;
 
 '''
+
+
+class Triangulo:
+
+    def __init__(self, anga, angb, angc):
+        self.a = anga
+        self.b = angb
+        self.c = angc
+
+
+    def CheckTriangulo(self):
+        if (abs(self.b-self.c) < self.a and self.a < (self.b+self.c)) or (abs(self.a-self.c) < self.b and self.b < (self.a+self.c)) or (abs(self.a-self.b) < self.c and self.c < (self.a+self.b)):
+            print("O triângulo é real!")
+        
+        else:
+            print("O triângulo não existe.")
+            
+
+triangulo_teste = Triangulo(5, 4, 3)
+
+triangulo_teste.CheckTriangulo();
+
