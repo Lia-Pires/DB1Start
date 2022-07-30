1aea6ffb8735233ecc0853b25e938017f650289b
Lia Pires <liasth.pires@gmail.com>
2022-07-30 17:31:45 -0300

    Update ex002_classes.py

--- 06_classes/ex002_classes.py
+++ 06_classes/ex002_classes.py
@@ -3,3 +3,12 @@ Escreva uma classe em Python para converter um
 número romano em um número inteiro
 
 '''
+
+class Romano:
+
+    equivalente = {"I":1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+
+    def __init__(self, romano:str):
+        self.romano = romano
+
+    
